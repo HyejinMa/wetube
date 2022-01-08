67c0ed4ac766b5fae0835d2a3cea7320060e62b3
HyejinMa <mahj345@gmail.com>
2022-01-08 18:29:43 +0900

    왜 안 뜨는지 몰?루

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -68,19 +68,19 @@ export const postLogin = async (req, res) => {
 };
 
 export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    clientId: process.env.GH_CLIENT,
+    client_id: process.env.GH_CLIENT,
     allow_signup: false,
     scope: "read:user user:email",
   };
   const params = new URLSearchParams(config).toString();
-  const baseUrl = "https://github.com/login/oauth/authorize?&{params}";
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
 
 export const finishGithubLogin = async (req, res) => {
-  const baseUrl = "https://github.com/login/oauth/authorize?&{params}";
+  const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
     client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
@@ -88,15 +88,37 @@ export const finishGithubLogin = async (req, res) => {
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
-  const data = await fetch(finalUrl, {
-    method: "POST",
-    headers: {
-      Accept: "application/json", // json으로 응답받기 위해서 이를 설정해야함
-    },
-  });
-  const json = await data.json();
-  console.log(json);
-  res.send(JSON.stringify(json));
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com/user";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userData);
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(emailData);
+  } else {
+    return res.redirect("/login");
+  }
 };
 
 export const edit = (req, res) => res.send("Edit User");
