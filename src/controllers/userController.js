a40cd9f287b4b18267e6e85c30901dfe4e41407f
HyejinMa <mahj345@gmail.com>
2022-03-08 18:11:32 +0900

    220308 css 다루기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -224,7 +224,10 @@ export const postChangePassword = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params; // 누구나 볼 수 있어야 하니 session 에서 id url을 갖고 오지 않는다.
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: { path: "owner", model: "User" },
+  });
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
