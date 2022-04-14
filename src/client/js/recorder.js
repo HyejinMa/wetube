4b9154389a305a8a34bffeeaeeaff207812df4ee
HyejinMa <mahj345@gmail.com>
2022-04-14 23:48:43 +0900

    220414 사용자의 카메라로부터 녹음을 시작하는 기능 녹음시작 버튼에  구현

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,9 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preivew");
+
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  video.srcObject = stream; // 파일이 아닌 src 객체.
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
