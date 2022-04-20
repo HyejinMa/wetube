984b881c0956088c88d64d39421cfb316cafbc70
HyejinMa <mahj345@gmail.com>
2022-04-20 22:56:46 +0900

    220420 api route1

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,21 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  // fetch는 JS를 통해서 request를 보낼 수 있게함
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      text,
+    },
+  });
 };
 
-form.addEventListener("submit", handleSubmit);
+// 사용자가 로그인이 되어있지 않으면, form을 불러올 수 없다.
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerView } from "../controllers/videoController.js";
+import { registerView, createComment } from "../controllers/videoController.js";
 
 const apiRouter = express.Router();
 
 // 프론트엔드에서 자바스크립트로 호출하는 URL
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 
 export default apiRouter;
